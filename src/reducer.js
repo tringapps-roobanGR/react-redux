2165477060cca39152ea041309f6c72232bc29ce
tringapps-roobanGR <96728739+tringapps-roobanGR@users.noreply.github.com>
2022-05-06 15:37:40 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,7 @@ const reducer= (action,state=initialState) => {
 
   switch (action.type) {
     case 'ADD':
-    return [action.task,action.task,initialState[2],initialState[3],initialState[4],]
+    return [action.task,action.task,initialState[2],initialState[3],initialState[4]]
 
     default:
       return initialState
